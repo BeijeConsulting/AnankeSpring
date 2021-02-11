f9b80e02feab989ba376a8036867bb703a5e58eb
marcoimperiale97 <marcoimpe.lav@gmail.com>
2021-02-11 15:36:47 +0100

    primo commit imperiale spring

--- src/it/beije/ananke/controller/FirstController.java
+++ src/it/beije/ananke/controller/FirstController.java
@@ -11,17 +11,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
 @Controller
 public class FirstController {
 
 	@RequestMapping(value = {"/", "index", "pippopluto"}, method = RequestMethod.GET)
 	public String index(HttpServletRequest request, Model model, Locale locale) {
 		System.out.println("sono nella index..." + request.getRequestURL() + " - locale " + locale.getDisplayCountry());
-		
 		String saluto = "Buongiorno";
 		model.addAttribute("saluto", saluto);
-		
 		return "index";
 	}
 	
@@ -30,5 +27,11 @@ public class FirstController {
 		System.out.println("sono nella hello...");
 		return "hello";
 	}
-
+	
+	@RequestMapping(value="/test", method = RequestMethod.GET)
+	public String test(Model model) {
+		System.out.println("Vado nella JSP di test");
+		model.addAttribute("test", "questo è un test di prova");
+		return "test";
+	}
 }
