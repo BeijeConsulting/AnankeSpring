6fa6d6d4ff43df807be7e8582a83037dcd2b461a
ivfly <iv0fly@gmail.com>
2021-02-11 12:01:43 +0100

    init Spring Project

---
+++ src/it/beije/ananke/controller/FirstController.java
@@ -0,0 +1,34 @@
+package it.beije.ananke.controller;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class FirstController {
+
+	@RequestMapping(value = {"/", "index", "pippopluto"}, method = RequestMethod.GET)
+	public String index(HttpServletRequest request, Model model, Locale locale) {
+		System.out.println("sono nella index..." + request.getRequestURL() + " - locale " + locale.getDisplayCountry());
+		
+		String saluto = "Buongiorno";
+		model.addAttribute("saluto", saluto);
+		
+		return "index";
+	}
+	
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String hello() {
+		System.out.println("sono nella hello...");
+		return "hello";
+	}
+
+}
