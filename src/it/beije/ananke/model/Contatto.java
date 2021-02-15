534af9f9c943bbb368e92d1fff885b1c0e84c3bb
ivfly <iv0fly@gmail.com>
2021-02-15 17:09:21 +0100

    + Spring Data

--- src/it/beije/ananke/model/Contatto.java
+++ src/it/beije/ananke/model/Contatto.java
@@ -1,23 +1,39 @@
 package it.beije.ananke.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+
+@Entity
+@Table(name="contatti")
 public class Contatto {
 	
-	private int id;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private Integer id;
 
+	@Column(name="name")
 	private String name;
 
+	@Column(name="surname")
 	private String surname;
 
+	@Column(name="telephone")
 	private String telephone;
 
+	@Column(name="email")
 	private String email;
 	
 	
-	public int getId() {
+	public Integer getId() {
 		return id;
 	}
-	public void setId(int id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 
@@ -49,12 +65,15 @@ public class Contatto {
 		this.email = email;
 	}
 	
-	public String getFullname() {
-		return name + ' ' + surname;
-	}
-	
 	
 	public String toString() {
+//		StringBuilder builder = new StringBuilder("[");
+//		builder.append("name : ").append(name);
+//		builder.append("surname : ").append(surname);
+//		builder.append("telephone : ").append(telephone);
+//		builder.append("email : ").append(email);
+//		builder.append("]");
+		
 		StringBuilder builder = new StringBuilder("[")
 				.append("id : ").append(id)
 				.append(", name : ").append(name)
