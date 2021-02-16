e09fcb639296009f1f5166df7588292f46f2193c
StfMari <stefanofrancesco.marinoni@gmail.com>
2021-02-16 12:23:15 +0100

    Controllers

--- src/it/beije/ananke/ecommerce/controllers/ControllerEcommerceOrder.java
+++ src/it/beije/ananke/ecommerce/controllers/ControllerEcommerceOrder.java
@@ -1,8 +1,14 @@
 package it.beije.ananke.ecommerce.controllers;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import it.beije.ananke.ecommerce.beans.User;
 import it.beije.ananke.repository.ContattoRepository;
 
 @Controller
@@ -15,5 +21,11 @@ public class ControllerEcommerceOrder {
 	//gestisce l'ordine di un cliente
 	//quindi quando riceve i dati di un prodotto da aggiungere al carrello
 	//prende i dati e li invia 
+	
+	@RequestMapping(value = "/ecommerce/addOrder", method = RequestMethod.POST)
+	public String postAddProduct(User user, Model model, HttpSession session) {
+		
+		return "ecommerceHomePage";
+	}
 
 }
