ed91bda4864e723489d2a03a016a6a7174ff159e
ivfly <iv0fly@gmail.com>
2021-02-17 16:08:42 +0100

    @OneToMany

--- src/it/beije/ananke/model/Contatto.java
+++ src/it/beije/ananke/model/Contatto.java
@@ -1,10 +1,17 @@
 package it.beije.ananke.model;
 
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 
@@ -23,11 +30,15 @@ public class Contatto {
 	@Column(name="surname")
 	private String surname;
 
-	@Column(name="telephone")
-	private String telephone;
+//	@Column(name="telephone")
+//	private String telephone;
 
 	@Column(name="email")
 	private String email;
+	
+	@OneToMany(fetch = FetchType.EAGER)
+	@JoinColumn(name="id_contatto")
+	private Set<Telefono> numeriTelefono;
 	
 	
 	public Integer getId() {
@@ -51,12 +62,12 @@ public class Contatto {
 		this.surname = surname;
 	}
 	
-	public String getTelephone() {
-		return telephone;
-	}
-	public void setTelephone(String telephone) {
-		this.telephone = telephone;
-	}
+//	public String getTelephone() {
+//		return telephone;
+//	}
+//	public void setTelephone(String telephone) {
+//		this.telephone = telephone;
+//	}
 	
 	public String getEmail() {
 		return email;
@@ -65,20 +76,21 @@ public class Contatto {
 		this.email = email;
 	}
 	
+	public Set<Telefono> getNumeriTelefono() {
+		return numeriTelefono;
+	}
+	public void setNumeriTelefono(Set<Telefono> numeriTelefono) {
+		this.numeriTelefono = numeriTelefono;
+	}
 	
 	public String toString() {
-//		StringBuilder builder = new StringBuilder("[");
-//		builder.append("name : ").append(name);
-//		builder.append("surname : ").append(surname);
-//		builder.append("telephone : ").append(telephone);
-//		builder.append("email : ").append(email);
-//		builder.append("]");
 		
 		StringBuilder builder = new StringBuilder("[")
 				.append("id : ").append(id)
 				.append(", name : ").append(name)
 				.append(", surname : ").append(surname)
-				.append(", telephone : ").append(telephone)
+//				.append(", telephone : ").append(telephone)
+				.append(", telephone numbers : ").append(getNumeriTelefono())
 				.append(", email : ").append(email).append("]");
 		
 		return builder.toString();
