8008ed077b9a4bb311d7d9873a6a623fbe6db74c
Padawan04 <demonacosara@gmail.com>
2021-02-18 18:02:04 +0100

    first methods

---
+++ src/it/beije/ananke/controller/CommerceRestController.java
@@ -0,0 +1,100 @@
+package it.beije.ananke.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import it.beije.ananke.model.Cart;
+import it.beije.ananke.model.Order;
+import it.beije.ananke.model.OrderItem;
+import it.beije.ananke.model.User;
+import it.beije.ananke.repository.OrderItemRepository;
+import it.beije.ananke.repository.OrderRepository;
+import it.beije.ananke.repository.ProductRepository;
+import it.beije.ananke.repository.UserRepository;
+import it.beije.ananke.service.CartService;
+
+@RestController
+@RequestMapping("/commerce")
+public class CommerceRestController {
+	
+	@Autowired 
+	private UserRepository userRepository;
+	
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	@Autowired
+	private ProductRepository pr;
+	
+	@Autowired 
+	private OrderItemRepository orderItemRepository;
+	
+	@Autowired
+	private CartService cartService;
+	
+	@GetMapping("/users")
+	public List<User> showUsers(){
+		List<User> list = userRepository.findAll();
+		return list;
+	}
+	
+	
+	@GetMapping("/orders/user/{id}")
+	public List<Order> showProdcuts(@PathVariable Integer id){
+		List<Order> list = orderRepository.findByStateAndUserId("open", id);
+		return list;
+	}
+	
+	
+	@GetMapping("/cart/user/{id}")
+	public Cart cartState(@PathVariable Integer id) {
+		Cart cart = cartService.showCart(id + "");
+		return cart;
+	}
+	
+	
+//	@PostMapping("/cart/product/{id}")
+//	public Cart addToCart(@RequestBody User user, @PathVariable Integer id) {
+//		Cart cart =  cartService.addToCart(user.getId() + "", id + "");
+//		return cart;
+//	}
+
+	
+	@PostMapping("cart/aggiungi/product/{id}")
+	public Cart addToCart(@PathVariable Integer id, HttpServletRequest request) {
+		HttpSession session = request.getSession();
+//		User user = (User) session.getAttribute("user");
+		User user = userRepository.getOne(1);
+		
+		Cart cart =  cartService.addToCart(user.getId() + "", id + "");
+//		Cart cart =  cartService.addToCart(1 + "", id + "");
+		return cart;
+	}
+	
+	
+	@PostMapping("cart/rimuovi/orderitem/{id}")
+	public Cart removeFromCart(@PathVariable Integer id, HttpServletRequest request) {
+//		HttpSession session = request.getSession();
+//		User user = (User) session.getAttribute("user");
+		User user = userRepository.getOne(1);
+		
+		Cart cart =  cartService.removeFromCart(user.getId() + "", id + "");
+//		Cart cart =  cartService.addToCart(1 + "", id + "");
+		return cart;
+	}
+	
+	
+	
+	
+}
--- src/it/beije/ananke/repository/UserRepository.java
+++ src/it/beije/ananke/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package it.beije.ananke.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,4 +14,6 @@ public interface UserRepository extends JpaRepository<User, Integer>{
 	public User save(User user);
 	
 	public Optional<User> findById(Integer id);
+	
+	public List<User> findAll();
 }
